12569b3aee573ac5dd1732d929c69b690ac53a38
Codedoffut <115214289+Codedoffut@users.noreply.github.com>
2022-10-09 15:35:10 +0100

    Add files via upload

--- hng3.js
+++ hng3.js
@@ -27,56 +27,30 @@ function showSlides(n) {
 } 
 
 
-var slideIndex = 1;
+// var slideIndex = 1;
 
-displaySlides(slideIndex);
+// displaySlides(slideIndex);
 
-function plusSlides(n) {
-  displaySlides(slideIndex += n);
-}
-
-function ourSlide(n) {
-  displaySlides(slideIndex = n);
-}
-
-function displaySlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("ourSlides");
-  var dots = document.getElementsByClassName("points");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "flex";  
-  dots[slideIndex-1].className += " active";
-} 
-
-
-
-// var imageSlider = 1
-// showThis(imageSlider);
-// function addSlides(b) {
-//     showThis(imageSlider += b);
+// function plusSlides(n) {
+//   displaySlides(slideIndex += n);
 // }
-// function mySlide(b) {
-//     showThis(imageSlider = b);
+
+// function ourSlide(n) {
+//   displaySlides(slideIndex = n);
 // }
-// function showThis(b) {
-//    var c;
-//    var mine= document.getElementsByClassName("mySlides") 
-//    var ours= document.getElementsByClassName("point")
-//    if (b > mine.length) {imageSlider = 1}    
-//   if (b < 1) {imageSlider = mine.length}
-//   for (c = 0; i < mine.length; c++) {
-//       mine[c].style.display = "none";  
+
+// function displaySlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("ourSlides");
+//   var dots = document.getElementsByClassName("points");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
 //   }
-//   for (c = 0; i < ours.length; c++) {
-//       ours[c].className = ours[c].className.replace(" active", "");
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
 //   }
-//   mine[imageSlider-1].style.display = "flex";  
-//   ours[imageSlider-1].className += " active";
-// }
+//   slides[slideIndex-1].style.display = "flex";  
+//   dots[slideIndex-1].className += " active";
+// } 
